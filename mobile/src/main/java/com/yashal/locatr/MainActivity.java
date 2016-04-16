a9eaee01206b144ce7392d3023063c116db55499
varshameghana <varshameghana30@gmail.com>
2016-04-16 16:08:57 +0530

    Add data base support

--- mobile/src/main/java/com/yashal/locatr/MainActivity.java
+++ mobile/src/main/java/com/yashal/locatr/MainActivity.java
@@ -1,7 +1,13 @@
 package com.yashal.locatr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.yashal.locatr.sections.contact_selector_activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button selectmainbutton = (Button) findViewById(R.id.select_contacts_button);
+        selectmainbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, contact_selector_activity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
--- mobile/src/main/java/com/yashal/locatr/sections/contact_selector_activity.java
+++ mobile/src/main/java/com/yashal/locatr/sections/contact_selector_activity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.yashal.locatr.R;
 
 public class contact_selector_activity extends AppCompatActivity {
@@ -15,6 +19,23 @@ public class contact_selector_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_selector);
+
+        Firebase.setAndroidContext(this);
+        Firebase myFirebaseRef = new Firebase("https://locatr.firebaseio.com/");
+
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+            }
+
+            @Override public void onCancelled(FirebaseError error) { }
+
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
